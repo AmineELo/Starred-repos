c1a07e0b392be35c73eb116222a0d876fae0dc85
Amine Elouattar <elouattar@gmail.com>
2018-07-23 11:43:56 +0100

    initialize the dagger appcomponent in the application entry and add it to the manifest.

--- app/src/main/java/com/example/amineelouattar/starredrepos/AppEntry.java
+++ app/src/main/java/com/example/amineelouattar/starredrepos/AppEntry.java
@@ -2,5 +2,25 @@ package com.example.amineelouattar.starredrepos;
 
 import android.app.Application;
 
+import com.example.amineelouattar.starredrepos.data.component.AppComponent;
+import com.example.amineelouattar.starredrepos.data.component.DaggerAppComponent;
+import com.example.amineelouattar.starredrepos.data.module.AppModule;
+
 public class AppEntry extends Application {
+
+    private AppComponent appComponent;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        appComponent = DaggerAppComponent
+                .builder()
+                .appModule(new AppModule(this))
+                .build();
+    }
+
+    public AppComponent getAppComponent() {
+        return appComponent;
+    }
 }
