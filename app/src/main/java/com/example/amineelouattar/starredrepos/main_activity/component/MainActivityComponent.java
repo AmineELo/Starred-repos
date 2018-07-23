737bf0b63c6154840ed68297ca1dc3d6a4b65a43
Amine Elouattar <elouattar@gmail.com>
2018-07-23 12:03:36 +0100

    add context module to provide context to the presenter.

--- app/src/main/java/com/example/amineelouattar/starredrepos/main_activity/component/MainActivityComponent.java
+++ app/src/main/java/com/example/amineelouattar/starredrepos/main_activity/component/MainActivityComponent.java
@@ -2,13 +2,14 @@ package com.example.amineelouattar.starredrepos.main_activity.component;
 
 import com.example.amineelouattar.starredrepos.data.component.AppComponent;
 import com.example.amineelouattar.starredrepos.main_activity.MainActivity;
+import com.example.amineelouattar.starredrepos.main_activity.module.ContextModule;
 import com.example.amineelouattar.starredrepos.main_activity.module.MainActivityModule;
 import com.example.amineelouattar.starredrepos.utils.ActivityScope;
 
 import dagger.Component;
 
 @ActivityScope
-@Component(dependencies = AppComponent.class, modules = {MainActivityModule.class})
+@Component(dependencies = AppComponent.class, modules = {MainActivityModule.class, ContextModule.class})
 public interface MainActivityComponent {
     void inject(MainActivity mainActivity);
 }
